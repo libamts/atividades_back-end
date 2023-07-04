e92eed137580152a648fcba0be530e71f2f840d2
Seu nome <liba@exemplo.com>
2023-07-03 21:34:55 -0300

    Adicionando atividade

---
+++ mod5/src/br/com/libamts/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.libamts;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello, Matheus!");
+	}
+
+}
