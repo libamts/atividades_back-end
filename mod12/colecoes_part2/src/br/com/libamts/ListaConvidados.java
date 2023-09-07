4d1647cb5bad6d79296276c5e41795414acfd26e
libamts <libamts@exemplo.com>
2023-09-07 18:23:45 -0300

    Mudando texto do else de novo

--- mod12/colecoes_part2/src/br/com/libamts/ListaConvidados.java
+++ mod12/colecoes_part2/src/br/com/libamts/ListaConvidados.java
@@ -39,7 +39,7 @@ public class ListaConvidados {
                 lista1.add(name);
 
             } else {
-                System.out.println("Convidado não adicionado! Não digitou um número válido.");
+                System.out.println("Convidado não adicionado! Você Não digitou um número válido.");
             }
 
             System.out.println("Você deseja adicionar mais algum convidado?");
